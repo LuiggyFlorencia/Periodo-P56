b2b0a8f4b152c443c8d2b903b6d50c9fef0c678b
LuiggyFlorencia <lflorencia@est.ups.edu.ec>
2020-10-01 11:09:04 -0500

    Se separa la parte de las respuestas por partes del servidor

--- Code/node-fundamentals/01_server/server.js
+++ Code/node-fundamentals/01_server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const response = require('./network/response')
 
 const router = express.Router()
 
@@ -9,14 +10,14 @@ app.use( bodyParser.urlencoded( {extended:false} ) )
 app.use( router )
 
 router.get('/carrera', function(req, res) {
-    res.send('Lista de Carreras de la UPS.')
+    response.success( req, res, 'Lista de Carreras de la UPS.', 200 )
 })
-
 router.post('/carrera', function(req, res) {
-    console.log(req.body)
-    console.log(req.query)
-
-    res.status(201).send({tipo_error:0, mensaje_error:'', mensaje_exito:'Añadido exitosamente.'})
+    if (req.query.error == 'ok') {
+        response.error( req, res, 'Error al ingresar la Carrera.', 500 )        
+    } else {
+        response.success( req, res, 'Ingreso de Carrera exitoso.', 201 )        
+    }
 })
 
 app.use( '/', express.static('public') )
